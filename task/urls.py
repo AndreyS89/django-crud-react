f9007b153e82e4f25ed38d91affe71159806b2fa
Samarin <samarinandrey55@gmail.com>
2023-05-24 19:50:10 +0300

    urls

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework import routers
+from task import views
 
 # api version
 router = routers.DefaultRouter()
+router.register(r'task', views.TaskView, 'tasks')
 
 urlpatterns = [
     path("api/v1")
